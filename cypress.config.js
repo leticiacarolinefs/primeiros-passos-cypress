cd223b315e4f671b892266e45d9021c6874c5450
Letícia <leticia.carol@gmail.com>
2026-06-29 21:37:34 -0300

    base url added

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,9 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  allowCypressEnv: false,
-
   e2e: {
+    baseUrl: "https://opensource-demo.orangehrmlive.com/web/index.php",
+
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
