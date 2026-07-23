5553e9224ece8b04097964ee1ce25d259e09294c
Letícia <leticia.carol@gmail.com>
2026-07-22 21:29:55 -0300

    add page objects

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -1,50 +1,106 @@
 import userData from '../fixtures/user-data.json'
+import LoginPage from '../pages/loginPage'
+import DashboardPage from '../pages/dashboardPage'
+import MenuPage from '../pages/menuPage'
+
+
+
+const loginPage = new LoginPage()
+const dashboardPage = new DashboardPage()
+const menuPage = new MenuPage()
+
+
 
 describe('Orange HRM - Tests', () => {
 
   const selectorsList = {
-    usernameField: "[name='username']",
-    passwordField: "[name='password']",
-    loginButton: "[type='submit']",
-    sectionTitleTopBar: '.oxd-topbar-header-breadcrumb-module',
-    wrongCredentialAlert: "[role='alert']",
-    myInfoButton: "[href='/web/index.php/pim/viewMyDetails']",
+    
+    
+  
     firstNameField: "[name='firstName']",
     middleNameField: "[name='middleName']",
     lastNameField: "[name='lastName']",
     genericField: ".oxd-input--active",
     dateField: "[placeholder='yyyy-dd-mm']",
+    dropdownContainer: ".oxd-select-text",
+    dropdownOption: ".oxd-select-option",
     dateCloseButton: ".--close",
     submiteButton: "[type='submit']",
   }
-  
+
   it.only('User info update - Sucess', () => {
-    cy.visit('/auth/login')
-    cy.get(selectorsList.usernameField).type(userData.userSuccess.username)
-    cy.get(selectorsList.passwordField).type(userData.userSuccess.password)
-    cy.get(selectorsList.loginButton).click()
-    cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
-    cy.get(selectorsList.sectionTitleTopBar).contains('Dashboard')
-    cy.get(selectorsList.myInfoButton).click()
+
+    loginPage.accessLoginPage()
+    loginPage.loginWithAnyUser(userData.userSuccess.username, userData.userSuccess.password)
+
+    dashboardPage.checkDashboardPage()
+
+    menuPage.accessMyInfo()
+
+    
+    
+   
+
+    // LOGIN
+    /*
+    
+    
+
+    // CAMPOS DE TEXTO
     cy.get(selectorsList.firstNameField).clear().type('Letícia')
     cy.get(selectorsList.middleNameField).clear().type('Caroline')
     cy.get(selectorsList.lastNameField).clear().type('Freitas Silva')
-    //cy.get(selectorsList.genericField).eq(4).type('nicknameTest')
     cy.get(selectorsList.genericField).eq(3).clear().type('employeeId')
     cy.get(selectorsList.genericField).eq(4).clear().type('otherId')
     cy.get(selectorsList.genericField).eq(5).clear().type('123Test')
+
+    // License Expiry Date
     cy.get(selectorsList.dateField).eq(0).clear().type('2026-14-07')
-    cy.get(selectorsList.dateCloseButton).click()
+    cy.get(selectorsList.dateCloseButton).click({ force: true })
+
+    // SALVAR
     cy.get(selectorsList.submiteButton).eq(0).click()
-    cy.get('body').should('contain','Successfully Updated')
-    cy.get('.oxd-toast-close')
+    cy.get('body').should('contain', 'Successfully Updated')
+
+    // FECHAR TOAST E GARANTIR QUE SUMIU
+    cy.get('.oxd-toast-close').click()
+    cy.get('.oxd-toast').should('not.exist')
+
+    // DROPDOWN DE NACIONALIDADE
+    cy.get(selectorsList.dropdownContainer).eq(0)
+      .should('be.visible')
+      .click({ force: true })
+
+    cy.get('.oxd-select-dropdown', { timeout: 10000 })
+      .should('exist')
+      .and('be.visible')
+
+    cy.get(selectorsList.dropdownOption)
+      .contains('Algerian')
+      .click()
+
+    // DROPDOWN DE MARITAL STATUS
+    cy.get(selectorsList.dropdownContainer).eq(1)
+      .should('be.visible')
+      .click({ force: true })
+
+    cy.get('.oxd-select-dropdown', { timeout: 10000 })
+      .should('exist')
+      .and('be.visible')
+
+    cy.get(selectorsList.dropdownOption)
+      .contains('Single')
+      .click()
+
+    // Date of Birth
+    cy.get(selectorsList.dateField).eq(1).clear().type('1992-14-11')*/
   })
+
   it('Login - Fail', () => {
     cy.visit('/auth/login')
     cy.get(selectorsList.usernameField).type(userData.userFail.username)
     cy.get(selectorsList.passwordField).type(userData.userFail.password)
     cy.get(selectorsList.loginButton).click()
     cy.get(selectorsList.wrongCredentialAlert)
-    
   })
-}) 
+})
---
+++ cypress/pages/menuPage.js
@@ -0,0 +1,75 @@
+class MenuPage {
+
+  selectorsList() {
+
+    const selectors = {
+
+      adminButton: "[href='/web/index.php/admin/viewAdminModule']",
+      pimButton: "[href='/web/index.php/pim/viewPimModule']",
+      leaveButton: "[href='/web/index.php/leave/viewLeaveModule']",
+      timeButton: "[href='/web/index.php/time/viewTimeModule']",
+      recruitment: "[href='/web/index.php/recruitment/viewRecruitmentModule']",
+      myInfoButton: "[href='/web/index.php/pim/viewMyDetails']",
+      performanceButton: "[href='/web/index.php/performance/viewPerformanceModule']",
+      dashboardButton: "[href='/web/index.php/dashboard/index']",
+      directoryButton: "[href='/web/index.php/directory/viewDirectory']",
+      maintenanceButton: "[href='/web/index.php/maintenance/viewMaintenanceModule']",
+      claimButton: "[href='/web/index.php/claim/viewClaimModule']",
+      buzzButton: "[href='/web/index.php/buzz/viewBuzz']"
+
+    }
+
+    return selectors
+
+  }
+
+  accessAdmin() {
+    cy.get(this.selectorsList().adminButton).click()
+  }
+
+  accessPim() {
+    cy.get(this.selectorsList().pimButton).click()
+  }
+
+  accessLeave() {
+    cy.get(this.selectorsList().leaveButton).click()
+  }
+
+  accessTime() {
+    cy.get(this.selectorsList().leaveButton).click()
+  }
+
+  accessRecruitment() {
+    cy.get(this.selectorsList().recruitment).click()
+  }
+
+  accessMyInfo() {
+    cy.get(this.selectorsList().myInfoButton).click()
+  }
+
+  accessPerformance() {
+    cy.get(this.selectorsList().performanceButton).click()
+  }
+
+  accessDashboard() {
+    cy.get(this.selectorsList().dashboardButton).click()
+  }
+
+  accessDirectory() {
+    cy.get(this.selectorsList().directoryButton).click()
+  }
+
+  accessMaintenance() {
+    cy.get(this.selectorsList().maintenanceButton).click()
+  }
+
+  accessClaim() {
+    cy.get(this.selectorsList().claimButton).click()
+  }
+
+  accessBuzz() {
+    cy.get(this.selectorsList().buzzButton).click()
+  }
+}
+
+export default MenuPage
