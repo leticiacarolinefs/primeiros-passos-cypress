7c5a5fdf202bc62ad070e94ef0a3b384c9758e20
Letícia <leticia.carol@gmail.com>
2026-07-31 11:57:51 -0300

    add ramdon values on user spec with chancejs

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -2,33 +2,20 @@ import userData from '../fixtures/user-data.json'
 import LoginPage from '../pages/loginPage'
 import DashboardPage from '../pages/dashboardPage'
 import MenuPage from '../pages/menuPage'
+import MyInfoPage from '../pages/myInfoPage'
 
+const Chance = require('chance')
 
-
+const chance = new Chance()
 const loginPage = new LoginPage()
 const dashboardPage = new DashboardPage()
 const menuPage = new MenuPage()
-
+const myInfoPage = new MyInfoPage()
 
 
 describe('Orange HRM - Tests', () => {
 
-  const selectorsList = {
-    
-    
-  
-    firstNameField: "[name='firstName']",
-    middleNameField: "[name='middleName']",
-    lastNameField: "[name='lastName']",
-    genericField: ".oxd-input--active",
-    dateField: "[placeholder='yyyy-dd-mm']",
-    dropdownContainer: ".oxd-select-text",
-    dropdownOption: ".oxd-select-option",
-    dateCloseButton: ".--close",
-    submiteButton: "[type='submit']",
-  }
-
-  it.only('User info update - Sucess', () => {
+ it('User info update - Sucess', () => {
 
     loginPage.accessLoginPage()
     loginPage.loginWithAnyUser(userData.userSuccess.username, userData.userSuccess.password)
@@ -37,70 +24,13 @@ describe('Orange HRM - Tests', () => {
 
     menuPage.accessMyInfo()
 
-    
-    
-   
+    myInfoPage.fillPersonalDetails(chance.first(), chance.first(), chance.last())
+    myInfoPage.fillEmployeeDetails('EmployeeId', 'otherId', '123', '2020-07-23')
+    myInfoPage.fillStatus()
+    myInfoPage.saveForm()
 
-    // LOGIN
-    /*
-    
-    
-
-    // CAMPOS DE TEXTO
-    cy.get(selectorsList.firstNameField).clear().type('Letícia')
-    cy.get(selectorsList.middleNameField).clear().type('Caroline')
-    cy.get(selectorsList.lastNameField).clear().type('Freitas Silva')
-    cy.get(selectorsList.genericField).eq(3).clear().type('employeeId')
-    cy.get(selectorsList.genericField).eq(4).clear().type('otherId')
-    cy.get(selectorsList.genericField).eq(5).clear().type('123Test')
-
-    // License Expiry Date
-    cy.get(selectorsList.dateField).eq(0).clear().type('2026-14-07')
-    cy.get(selectorsList.dateCloseButton).click({ force: true })
-
-    // SALVAR
-    cy.get(selectorsList.submiteButton).eq(0).click()
-    cy.get('body').should('contain', 'Successfully Updated')
-
-    // FECHAR TOAST E GARANTIR QUE SUMIU
-    cy.get('.oxd-toast-close').click()
-    cy.get('.oxd-toast').should('not.exist')
-
-    // DROPDOWN DE NACIONALIDADE
-    cy.get(selectorsList.dropdownContainer).eq(0)
-      .should('be.visible')
-      .click({ force: true })
-
-    cy.get('.oxd-select-dropdown', { timeout: 10000 })
-      .should('exist')
-      .and('be.visible')
-
-    cy.get(selectorsList.dropdownOption)
-      .contains('Algerian')
-      .click()
-
-    // DROPDOWN DE MARITAL STATUS
-    cy.get(selectorsList.dropdownContainer).eq(1)
-      .should('be.visible')
-      .click({ force: true })
-
-    cy.get('.oxd-select-dropdown', { timeout: 10000 })
-      .should('exist')
-      .and('be.visible')
-
-    cy.get(selectorsList.dropdownOption)
-      .contains('Single')
-      .click()
-
-    // Date of Birth
-    cy.get(selectorsList.dateField).eq(1).clear().type('1992-14-11')*/
+  
   })
 
-  it('Login - Fail', () => {
-    cy.visit('/auth/login')
-    cy.get(selectorsList.usernameField).type(userData.userFail.username)
-    cy.get(selectorsList.passwordField).type(userData.userFail.password)
-    cy.get(selectorsList.loginButton).click()
-    cy.get(selectorsList.wrongCredentialAlert)
-  })
+  
 })
---
+++ cypress/pages/myInfoPage.js
@@ -0,0 +1,75 @@
+class MyInfoPage {
+
+  selectorsList() {
+
+    const selectors = {
+
+      firstNameField: "[name='firstName']",
+      middleNameField: "[name='middleName']",
+      lastNameField: "[name='lastName']",
+      genericField: ".oxd-input--active",
+      dateField: "[placeholder='yyyy-dd-mm']",
+      dropdownContainer: ".oxd-select-text",
+      dropdownOption: ".oxd-select-option",
+      dateCloseButton: ".--close",
+      submiteButton: "[type='submit']",
+
+    }
+
+    return selectors
+
+  }
+
+  fillPersonalDetails(firstName, midleName, lastName) {
+    cy.get(this.selectorsList().firstNameField).clear().type(firstName)
+    cy.get(this.selectorsList().middleNameField).clear().type(midleName)
+    cy.get(this.selectorsList().lastNameField).clear().type(lastName)
+  }
+
+  fillEmployeeDetails(employeeId, otherId, driversLicenseNumber, driversLicenseDate) {
+    cy.get(this.selectorsList().genericField).eq(3).clear().type(employeeId)
+    cy.get(this.selectorsList().genericField).eq(4).clear().type(otherId)
+    cy.get(this.selectorsList().genericField).eq(5).clear().type(driversLicenseNumber)
+    cy.get(this.selectorsList().dateField).eq(0).clear().type(driversLicenseDate)
+    cy.get(this.selectorsList().dateCloseButton).click({ force: true })
+  }
+
+saveForm() {
+  cy.get(this.selectorsList().submiteButton).eq(0).click({force: true})
+  cy.get('body').should('contain', 'Successfully Updated')
+  cy.get('.oxd-toast-close')
+}
+
+fillStatus() {
+  // DROPDOWN DE NACIONALIDADE
+    cy.get(this.selectorsList().dropdownContainer).eq(0)
+      .should('be.visible')
+      .click({ force: true })
+
+    cy.get('.oxd-select-dropdown', { timeout: 10000 })
+      .should('exist')
+      .and('be.visible')
+
+    cy.get(this.selectorsList().dropdownOption)
+      .contains('Algerian')
+      .click()
+
+    // DROPDOWN DE MARITAL STATUS
+    cy.get(this.selectorsList().dropdownContainer).eq(1)
+      .should('be.visible')
+      .click({ force: true })
+
+    cy.get('.oxd-select-dropdown', { timeout: 10000 })
+      .should('exist')
+      .and('be.visible')
+
+    cy.get(this.selectorsList().dropdownOption)
+      .contains('Single')
+      .click()
+}
+
+
+}
+
+
+export default MyInfoPage
